8df2cc54869ce6a3f4a2342f632dbaefb19a6da2
Arshad-17 <66469844+Arshad-17@users.noreply.github.com>
2020-07-04 18:03:17 +0530

    Create Audio_feature_Extraction.py

---
+++ Audio_feature_Extraction.py
@@ -0,0 +1,45 @@
+import soundfile as sf
+filename = "sample raw voice.wav"
+data, fs = sf.read(filename, dtype='float32')
+print(data,fs)
+
+def spectral_statistics(y, fs: int) -> dict:
+    spec = np.abs(np.fft.rfft(y))
+    freq = np.fft.rfftfreq(len(y), d=1 / fs)
+    spec = np.abs(spec)
+    amp = spec / spec.sum()
+    mean = (freq * amp).sum()
+    sd = np.sqrt(np.sum(amp * ((freq - mean) ** 2)))
+    amp_cumsum = np.cumsum(amp)
+    median = freq[len(amp_cumsum[amp_cumsum <= 0.5]) + 1]
+    mode = freq[amp.argmax()]
+    Q25 = freq[len(amp_cumsum[amp_cumsum <= 0.25]) + 1]
+    Q75 = freq[len(amp_cumsum[amp_cumsum <= 0.75]) + 1]
+    IQR = Q75 - Q25
+    z = amp - amp.mean()
+    w = amp.std()
+    skew = ((z ** 3).sum() / (len(spec) - 1)) / w ** 3
+    kurt = ((z ** 4).sum() / (len(spec) - 1)) / w ** 4
+
+    
+    result_d = {
+        'meanfreq': mean,
+        'sd': sd,
+        'median': median,
+        'mode': mode,
+        'Q25': Q25,
+        'Q75': Q75,
+        'IQR': IQR,
+        'skew': skew,
+        'kurt': kurt}
+
+    return(result_d)
+
+dictionary = spectral_statistics(data,fs,lowcut)
+print(dictionary)
+
+
+testvoice = pd.DataFrame()
+testvoice= voice.append(dictionary, ignore_index=True)
+testvoice.reset_index()
+testvoice.to_csv('test1.csv')
